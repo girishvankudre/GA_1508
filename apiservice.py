942b35178ab467729bdcf01abd6276b5e1a222fc
girishvankudre <48940191+girishvankudre@users.noreply.github.com>
2020-08-15 17:20:28 +0530

    Update apiservice.py

--- apiservice.py
+++ apiservice.py
@@ -15,5 +15,18 @@ def summarise():
     summaryText = summaryObj.summarise()
     return summaryText
 
+@app.route('/get_Post_summary',methods=['POST'])
+def summarise_post():
+    article = json.loads(request.data.decode())['article']
+    summaryObjPost = FindSummary('../GA_1508/config')
+    summaryTextPost = summaryObjPost.summarise_post(article)
+    return summaryTextPost
+
+@app.route('/about_us',methods=['POST'])
+def dummyReq():
+    article = json.loads(request.data.decode())['article']
+    print('Printing data:\n',article)
+    return "Ok request works"
+
 if __name__=="__main__":
     app.run(host='127.0.0.1',port=8080,debug=True)
