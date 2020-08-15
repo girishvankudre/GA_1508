fe9642f6e2938407df9459457c40dc24965c4309
girishvankudre <48940191+girishvankudre@users.noreply.github.com>
2020-08-15 15:09:23 +0530

    Add files via upload

---
+++ ga_1508_textpreprocess.py
@@ -0,0 +1,34 @@
+import re
+
+class PreprocessText:
+    """
+    Preprocess articles fed from files
+    """
+    
+    def __init__(self):
+        pass
+    
+    def convertToLower(self,text):
+        """
+        Convert text to lowercase
+        
+        Input:
+            text: string
+        Output:
+            text:string
+        """
+        loweredText = text.lower()
+        return loweredText
+    
+    def removeSpecialChar(self,text):
+        """
+        """
+        processedText = re.sub(',|;|<|>|','',text)
+        return processedText
+
+if __name__=="__main__":
+    preprocessObj = PreprocessText()
+    text = "Jamaican Foreign Minister Kamina Smith had wished Ms. Harris on her “historic selection” in a tweet."
+    loweredText = preprocessObj.convertToLower(text)
+    filteredText = preprocessObj.removeSpecialChar(loweredText)
+    print('filtered text: {}'.format(filteredText))
